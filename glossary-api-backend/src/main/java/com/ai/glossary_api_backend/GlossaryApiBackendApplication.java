f2ee18093e8c981f258614d1d292ee38f863f02e
Shags98 <shagithiya87@gmail.com>
2026-03-11 11:16:03 -0700

    Initial Spring Boot Project from start.spring.io

---
+++ glossary-api-backend/src/main/java/com/ai/glossary_api_backend/GlossaryApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ai.glossary_api_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlossaryApiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlossaryApiBackendApplication.class, args);
+	}
+
+}
---
+++ glossary-api-backend/src/test/java/com/ai/glossary_api_backend/GlossaryApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.glossary_api_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlossaryApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
